9c547f750ce8e41981f299bfc5cc4441d66b6118
Tony Bark <35226681+tonytins@users.noreply.github.com>
2026-04-21 08:08:53 -0400

    More lobby code

--- LigerZero.Godot/scripts/Lobby.cs
+++ LigerZero.Godot/scripts/Lobby.cs
@@ -1,7 +1,3 @@
-using System;
-using Godot;
-using Godot.Collections;
-
 // Based on https://docs.godotengine.org/en/stable/tutorials/networking/high_level_multiplayer.html
 internal partial class Lobby : Node // I.e. map view
 {
@@ -25,6 +21,8 @@ internal partial class Lobby : Node // I.e. map view
 
     private Dictionary<long, Dictionary<string, string>> _players = new();
 
+    private int _playersLoaded = 0;
+
     public static Lobby Instance { get; private set; }
 
     public override void _Ready()
@@ -33,8 +31,59 @@ internal partial class Lobby : Node // I.e. map view
         Multiplayer.PeerConnected += OnPlayerConnected;
     }
 
+    private Error JoinGame(string ip = "")
+    {
+        if (string.IsNullOrEmpty(ip)) ip = DefaultServerIP;
+
+        var peer = new ENetMultiplayerPeer();
+        var error = peer.CreateClient(ip, Port);
+        if (error != Error.Ok) return error;
+
+        Multiplayer.MultiplayerPeer = peer;
+        return Error.Ok;
+    }
+
+    private Error CreateGame()
+    {
+        var peer = new ENetMultiplayerPeer();
+        var error = peer.CreateServer(Port, MaxConnections);
+
+        if (error != Error.Ok) return error;
+
+        Multiplayer.MultiplayerPeer = peer;
+        _players[1] = _playerInfo;
+        EmitSignal(SignalName.PlayerConnected, 1, _playerInfo);
+
+        return Error.Ok;
+    }
+
+    private void RemoveMultiplayerPeer()
+    {
+        Multiplayer.MultiplayerPeer = null;
+        _players.Clear();
+    }
+
+    // When the server decides to start the game from a scene
+    // do RPC(Lobby.MethodName.LoadGame, filepath);
+    [Rpc(CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    private void LoadGame(string scene)
+    {
+        GetTree().ChangeSceneToFile(scene);
+    }
+
+    // Every peer will callthis when they have loaded the game scene.
+    private void PlayerLoaded()
+    {
+        if (!Multiplayer.IsServer()) return;
+        _playersLoaded += 1;
+        if (_playersLoaded != _players.Count) return;
+        // Should be something like this: GetNode<Game>("/root/Game").StartGame();
+        GetNode("/root/Game");
+        _playersLoaded = 0;
+    }
+
     private void OnPlayerConnected(long id)
     {
-        throw new NotImplementedException();
+        // RpcId(id, MethodName.RegisterPlayer, _playerInfo);
     }
 }
